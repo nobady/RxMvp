256c84935b2d402336961aebee260e71ce4b7797
tengfei.lv <1374966541@qq.com>
2016-08-11 18:03:48 +0800

    init

--- rx_mvp_libs/src/main/java/tf/rx_mvp_libs/ui/HFRecyclerView.java
+++ rx_mvp_libs/src/main/java/tf/rx_mvp_libs/ui/HFRecyclerView.java
@@ -2,8 +2,12 @@ package tf.rx_mvp_libs.ui;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.view.ViewTreeObserver;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Scroller;
 
 /**
  * [Description]
@@ -15,15 +19,107 @@ import android.util.AttributeSet;
 
 public class HFRecyclerView extends RecyclerView {
 
+    private Scroller mScroller;
+    private HFRecyclerViewHeader recyclerViewHeader;
+    private HFRecyclerViewFooter recyclerViewFooter;
+
+    private HFRecyclerViewAdapter hfAdapter;
+    //数据观察者
+    private HFAdapterDataObserver dataObserver;
+
+    private boolean canLoadMore;
+
+    private boolean canRefresh;
+
+    private HFRecyclerViewItemClickListener itemClickListener;
+
     public HFRecyclerView (Context context) {
         super (context);
     }
 
     public HFRecyclerView (Context context, @Nullable AttributeSet attrs) {
         super (context, attrs);
+        init (context);
     }
 
     public HFRecyclerView (Context context, @Nullable AttributeSet attrs, int defStyle) {
         super (context, attrs, defStyle);
+    }
+
+    public void setCanLoadMore (boolean canLoadMore) {
+        this.canLoadMore = canLoadMore;
+    }
+
+    public void setCanRefresh (boolean canRefresh) {
+        this.canRefresh = canRefresh;
+    }
+
+    public void setItemClickListener (HFRecyclerViewItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    /**
+     * 初始化
+     * @param context
+     */
+    private void init (Context context) {
+        mScroller = new Scroller (context,new DecelerateInterpolator ());
+        recyclerViewHeader = new HFRecyclerViewHeader (context);
+        recyclerViewFooter = new HFRecyclerViewFooter (context);
+        recyclerViewHeader.getViewTreeObserver ().addOnGlobalLayoutListener (new ViewTreeObserver.
+                OnGlobalLayoutListener () {
+            @Override
+            public void onGlobalLayout () {
+            }
+        });
+        setLayoutManager (new LinearLayoutManager (context));
+        dataObserver = new HFAdapterDataObserver ();
+        setOverScrollMode (OVER_SCROLL_NEVER);
+    }
+    @Override
+    public void setAdapter (Adapter adapter) {
+        hfAdapter = new HFRecyclerViewAdapter (adapter);
+        adapter.registerAdapterDataObserver (dataObserver);
+        hfAdapter.setRecyclerViewHeader (recyclerViewHeader);
+        hfAdapter.setRecyclerViewFooter (recyclerViewFooter);
+        hfAdapter.setLoadMore (canLoadMore);
+        hfAdapter.setRefresh (canRefresh);
+        hfAdapter.setItemClickListener (itemClickListener);
+        super.setAdapter (hfAdapter);
+    }
+
+    /**
+     * 数据观察者，
+     */
+    class HFAdapterDataObserver extends AdapterDataObserver{
+        @Override
+        public void onChanged () {
+            hfAdapter.notifyDataSetChanged ();
+        }
+
+        @Override
+        public void onItemRangeChanged (int positionStart, int itemCount) {
+            hfAdapter.notifyItemRangeChanged (positionStart+hfAdapter.getHeadViewCount (),itemCount);
+        }
+
+        @Override
+        public void onItemRangeChanged (int positionStart, int itemCount, Object payload) {
+            hfAdapter.notifyItemRangeChanged (positionStart+hfAdapter.getHeadViewCount (),itemCount,payload);
+        }
+
+        @Override
+        public void onItemRangeInserted (int positionStart, int itemCount) {
+            hfAdapter.notifyItemRangeInserted (positionStart+hfAdapter.getHeadViewCount (),itemCount);
+        }
+
+        @Override
+        public void onItemRangeRemoved (int positionStart, int itemCount) {
+            hfAdapter.notifyItemRangeRemoved (positionStart+hfAdapter.getHeadViewCount (),itemCount);
+        }
+
+        @Override
+        public void onItemRangeMoved (int fromPosition, int toPosition, int itemCount) {
+            hfAdapter.notifyItemMoved (fromPosition+hfAdapter.getHeadViewCount (),toPosition+hfAdapter.getHeadViewCount ());
+        }
     }
 }
--- rx_mvp_libs/src/main/java/tf/rx_mvp_libs/ui/HFRecyclerViewAdapter.java
+++ rx_mvp_libs/src/main/java/tf/rx_mvp_libs/ui/HFRecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package tf.rx_mvp_libs.ui;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
 
 /**
@@ -27,6 +28,10 @@ public class HFRecyclerViewAdapter extends RecyclerView.Adapter {
      */
     private HFRecyclerViewFooter recyclerViewFooter;
     /**
+     * 一直在recyclerview上方的view
+     */
+    private View headView;
+    /**
      * item事件监听器
      */
     private HFRecyclerViewItemClickListener itemClickListener;
@@ -46,51 +51,170 @@ public class HFRecyclerViewAdapter extends RecyclerView.Adapter {
      * 列表foot个数
      */
     private int mFooterCount;
+    /**
+     * 具体业务的adapter
+     */
+    private RecyclerView.Adapter mAdapter;
 
 
+    public HFRecyclerViewAdapter (RecyclerView.Adapter mAdapter) {
+        this.mAdapter = mAdapter;
+    }
+
+    /**
+     * 设置自定义view
+     * @param headView
+     */
+    public void setHeadView (View headView) {
+        this.headView = headView;
+    }
+
+    /**
+     * 设置是否可以下拉刷新
+     * @param refresh  true 可以下拉
+     */
     public void setRefresh (boolean refresh) {
         isRefresh = refresh;
-        if(refresh){
+        if (refresh) {
             mHeaderCount = 1;
-        }else {
+        } else {
             mHeaderCount = 0;
         }
 
     }
 
+    /**
+     * 设置是否支持上拉加载
+     * @param loadMore  true  支持
+     */
     public void setLoadMore (boolean loadMore) {
         isLoadMore = loadMore;
-        if(loadMore){
+        if (loadMore) {
             mFooterCount = 1;
-        }else {
+        } else {
             mFooterCount = 0;
         }
     }
 
+    /**
+     * 设置头部view】
+     * @param recyclerViewHeader
+     */
     public void setRecyclerViewHeader (HFRecyclerViewHeader recyclerViewHeader) {
         this.recyclerViewHeader = recyclerViewHeader;
     }
 
+    /**
+     * 设置底部view
+     * @param recyclerViewFooter
+     */
     public void setRecyclerViewFooter (HFRecyclerViewFooter recyclerViewFooter) {
         this.recyclerViewFooter = recyclerViewFooter;
     }
 
+    /**
+     * 设置点击时间监听器
+     * @param itemClickListener
+     */
     public void setItemClickListener (HFRecyclerViewItemClickListener itemClickListener) {
         this.itemClickListener = itemClickListener;
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder (ViewGroup parent, int viewType) {
-        return null;
+        if (viewType == ITEM_RECYCLER_HEADER) {
+            return new HFHolder (recyclerViewHeader);
+        } else if (viewType == ITEM_RECYCLER_FOOTER) {
+            return new HFHolder (recyclerViewFooter);
+        } else if (viewType == ITEM_HEAD_VIEW) {
+            return new HFHolder (headView);
+        }
+        return mAdapter.onCreateViewHolder (parent, viewType);
     }
 
     @Override
-    public void onBindViewHolder (RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder (final RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof HFHolder) {
+            return;
+        }
+        //要将头部view的数量除去 才算实际 的position
+        int po = position - getHeadViewCount ();
+        mAdapter.onBindViewHolder (holder, po);
 
+        //设置长按短按监听
+        if (itemClickListener != null) {
+            holder.itemView.setOnClickListener (new View.OnClickListener () {
+                @Override
+                public void onClick (View v) {
+                    itemClickListener.onItemClick (v, holder.getLayoutPosition ());
+                }
+            });
+
+            holder.itemView.setOnLongClickListener (new View.OnLongClickListener () {
+                @Override
+                public boolean onLongClick (View v) {
+                    itemClickListener.onLongItemClick (v, holder.getLayoutPosition ());
+                    return true;
+                }
+            });
+        }
     }
 
     @Override
     public int getItemCount () {
-        return 0;
+        int itemCount = mAdapter.getItemCount ();
+        itemCount+=getHeadViewCount ();
+        if(isLoadMore){
+            itemCount+=mFooterCount;
+        }
+        return itemCount;
+    }
+
+    @Override
+    public int getItemViewType (int position) {
+        if(isRefresh&&isHeaderView (position)){
+            return ITEM_RECYCLER_HEADER;
+        }else if(isLoadMore&&isFootrtView (position)){
+            return ITEM_RECYCLER_FOOTER;
+        }else if(isCustomHeadView (position)){
+            return ITEM_HEAD_VIEW;
+        }
+        return ITEM_RECYCLER_CONTENT;
+    }
+
+    //判断当前item是否是头部view
+    private boolean isHeaderView (int position) {
+        return mHeaderCount != 0 && position == 0;
+    }
+    //判断当前item是否是底部view
+    private boolean isFootrtView (int position) {
+        return mFooterCount != 0 && position >= (mAdapter.getItemCount () + getHeadViewCount ());
+    }
+    //判断当前item是否是自定义view
+    private boolean isCustomHeadView (int position) {
+        return headView != null && position == 1;
+    }
+
+    /**
+     * 获取头部的view总数
+     *
+     * @return
+     */
+    public int getHeadViewCount () {
+        int count = 0;
+        if (isRefresh) {
+            count += 1;
+        }
+        if (headView != null) {
+            count += 1;
+        }
+        return count;
+    }
+
+    class HFHolder extends RecyclerView.ViewHolder {
+
+        public HFHolder (View itemView) {
+            super (itemView);
+        }
     }
 }
